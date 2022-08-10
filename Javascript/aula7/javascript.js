012508944d6f4d6daccc3568a63fc6e48677c5b4
Sanzappa <santiagozapparoli@gmail.com>
2022-08-10 11:20:26 -0300

    boas

---
+++ Javascript/aula7/javascript.js
@@ -0,0 +1,21 @@
+const Usuarios = [
+    {
+        nome :"Santiago",
+        nickname : "Santigas",
+        senha : "123"
+    },
+    {
+        nome :"Guilherme",
+        nickname : "Guiermo",
+        senha : "321"
+    },
+    {
+        nome :"Felipe",
+        nickname : "Felipe",
+        senha : "12345"
+    }
+]
+
+function btLogin() {
+    window.location.href = "./loja.html";
+}
