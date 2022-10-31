b8d5bb6a31ea516469bb56e43748a320b6db913d
Sanzappa <santiagozapparoli@gmail.com>
2022-10-31 11:19:20 -0200

    iha

---
+++ 2-DES/pokedex/script.js
@@ -0,0 +1,30 @@
+var page = -1
+
+function pokeload(){
+    page ++
+    const card = document.querySelector(".card")
+
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=16&offset=" + (page * 16))
+    .then(res => { return res.json() })
+    .then(item =>{
+        item.results.forEach(data =>{
+            
+            fetch(data.url)
+            .then(res => { return res.json() })
+            .then(item =>{
+
+                    let i = card.cloneNode(true)
+
+                    i.classList.remove("model")
+
+                    i.querySelector("#pokeImg").src = item.sprites.versions['generation-v']['black-white'].animated.front_default;
+
+                    document.querySelector(".page").appendChild(i)
+                    i.querySelector("#pokeName").innerHTML = data.name
+
+            })
+
+        })
+        
+    })
+}
